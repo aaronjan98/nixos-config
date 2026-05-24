5602e0399e40c4f54812bc3cc32768c32e8a9a63
aaronjan <aaronjan98@gmail.com>
2026-05-24 14:43:45 -0700

    update subnet mask in 'modules/wol-sauron.nix'

--- modules/wol-sauron.nix
+++ modules/wol-sauron.nix
@@ -4,7 +4,7 @@ let
   wol-sauron = pkgs.writeShellScriptBin "wol-sauron" ''
     #!/usr/bin/env sh
     set -eu
-    exec ${pkgs.wakeonlan}/bin/wakeonlan -i 192.168.1.255 -p 9 3C:52:82:74:03:F5
+    exec ${pkgs.wakeonlan}/bin/wakeonlan -i 10.0.50.255 -p 9 3C:52:82:74:03:F5
   '';
 in
 {
